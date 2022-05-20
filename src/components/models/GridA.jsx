e19f1567c1011b20a4ff5941f17b6345c2724809
Rocamain <fjrocavazquez@gmail.com>
2022-05-20 18:04:25 +0100

    makes reponsivefonts works, and start Grid A component

--- src/components/models/GridA.jsx
+++ src/components/models/GridA.jsx
@@ -1,5 +1,5 @@
 import useNearScreen from 'hooks/useNearScreen';
-import { Box, Typography, Grid } from '@mui/material';
+import { Box, Typography, Grid, Divider } from '@mui/material';
 
 export default function GridA({ id, data }) {
   const { fromRef, isNearScreen } = useNearScreen();
@@ -9,42 +9,36 @@ export default function GridA({ id, data }) {
       ref={fromRef}
       component={'section'}
       sx={{
-        marginBottom: '10vh',
-        // backgroundColor: '#75c9cc',
+        margin: '15vh auto',
       }}
     >
       {isNearScreen ? (
         <Box
           sx={{
-            // maxHeight: 'calc(100% - 88px) !important',
-            // overflow: 'auto',
-            // display: 'block',
-            margin: '-16% auto!important',
+            width: '100%',
+            backgroundImage:
+              'linear-gradient(90deg,#75c9cc 0%,#00bccc 100%)!important',
+            display: 'inline-block',
+            margin: '0 auto',
           }}
         >
-          <Grid
-            container
-            rowSpacing={{ xs: 2, sm: 2, md: '0' }}
-            // columnSpacing={{ md: 3 }}
-            direction={'row'}
-            // spacing={10}
-            component="div"
-            sx={{
-              height: '100%',
-              backgroundImage:
-                'linear-gradient(90deg,#75c9cc 0%,#00bccc 100%)!important',
-              backgroundColor: 'lightblue',
-              // position: 'relative',
-
-              // top: '-1em',
-              // zIndex: 1000,
-              maxWidth: '80vw',
-              boxShadow: 'rgba(56, 21, 11, 0.09) 0px 50px 80px 0px',
-            }}
-          >
-            <GridText data={data} />
-            <GridCards data={data} />
-          </Grid>
+          <Box sx={{ width: '80%', margin: '-7em auto' }}>
+            <Grid
+              container
+              direction={'row'}
+              component="div"
+              sx={{
+                height: '100%',
+                backgroundColor: '#F4F9FC',
+                margin: '0 auto',
+                padding: '2em',
+                boxShadow: 'rgba(56, 21, 11, 0.09) 0px 50px 80px 0px',
+              }}
+            >
+              <GridText data={data} />
+              <GridCards data={data} />
+            </Grid>
+          </Box>
         </Box>
       ) : null}
     </Box>
@@ -56,13 +50,23 @@ const GridText = ({ data }) => {
     <Grid item component="div" xs={12} sm={12} md={6}>
       <Box
         sx={{
-          padding: '3em 0',
-          width: '90%',
+          width: '100%',
+          whiteSpace: 'pre-wrap',
           mx: 'auto',
+          padding: '3em 0',
+          textAlign: 'justify',
         }}
       >
         <Typography component="h2" variant="title" children={data.title} />
-        <Typography component="p" variant="gridText" children={data.content} />
+        <Divider
+          sx={{
+            margin: '1.5em 0 1.5em 0',
+            borderColor: '#ffd4a3',
+            borderBottomWidth: 'medium',
+            width: '60%',
+          }}
+        />
+        <Typography component="p" variant="content" children={data.content} />
       </Box>
     </Grid>
   );
@@ -77,69 +81,108 @@ const GridCards = ({ data }) => {
       sm={12}
       md={6}
       lg={6}
-      container={true}
-      spacing={4}
+      container
+      alignItems="flex-start"
+      columnSpacing={1}
+      rowSpacing={{ xs: 3, md: 5 }}
     >
-      <Grid item component="div" xs={12} sm={6} container={true} rowSpacing={3}>
+      <Grid item component="div" xs={12} sm={6} container={true} rowSpacing={2}>
         {data.cards.map(
           (card, index) =>
             index % 2 === 0 && (
               <Grid key={index} item>
-                <Box
-                  // elevation={24}
-                  sx={{
-                    backgroundColor: 'lightgoldenrodyellow',
-                    padding: '2em',
-                    alignItems: 'flex-start',
-                  }}
-                >
-                  <Box sx={{ width: '5em' }}>
+                <Box sx={{ width: '100%', margin: '0 auto' }}>
+                  <Box
+                    sx={{
+                      backgroundColor: 'red',
+                      borderRadius: '5px',
+                      alignItems: 'flex-start',
+                      padding: '2em 1em',
+                    }}
+                  >
                     <Box
-                      component="img"
-                      src={card.photo.url}
-                      sx={{ objectFit: 'cover', width: '100%' }}
+                      sx={{
+                        width: '70%',
+                        maxWidth: '7em',
+                        margin: '0 auto',
+                        marginBottom: '1em',
+                      }}
+                    >
+                      <Box
+                        component="img"
+                        src={card.photo.url}
+                        sx={{
+                          objectFit: 'cover',
+                          width: '100%',
+
+                          height: 'auto',
+                        }}
+                      />
+                    </Box>
+
+                    <Typography
+                      component="h1"
+                      variant="h4"
+                      children={card.title}
+                      sx={{ marginBottom: '0.7em', color: 'yellow' }}
+                    />
+                    <Typography
+                      component="p"
+                      children={card.content}
+                      style={{ whiteSpace: 'pre-wrap' }}
                     />
                   </Box>
-                  <Typography component="h2" variant="" children={card.title} />
-                  <Typography
-                    component="p"
-                    variant=""
-                    children={card.content}
-                  />
                 </Box>
               </Grid>
             )
         )}
       </Grid>
-      <Grid item component="div" xs={12} sm={6} container={true} rowSpacing={3}>
+      <Grid item component="div" xs={12} sm={6} container={true} rowSpacing={2}>
         {data.cards.map(
           (card, index) =>
             index % 2 !== 0 && (
               <Grid key={index} item>
-                <Box
-                  // elevation={24}
-                  sx={{
-                    backgroundColor: 'lightgoldenrodyellow',
-                    padding: '2em',
-                  }}
-                >
-                  <Box sx={{ width: '5em' }}>
+                <Box sx={{ width: '100%', margin: '0 auto' }}>
+                  <Box
+                    sx={{
+                      backgroundColor: 'red',
+                      borderRadius: '5px',
+                      alignItems: 'flex-start',
+                      padding: '2em 1em',
+                    }}
+                  >
                     <Box
-                      component="img"
-                      src={card.photo.url}
                       sx={{
-                        objectFit: 'cover',
-                        width: '100%',
-                        alignItems: 'flex-start',
+                        width: '70%',
+                        maxWidth: '7em',
+                        margin: '0 auto',
+                        marginBottom: '1em',
                       }}
+                    >
+                      <Box
+                        component="img"
+                        src={card.photo.url}
+                        sx={{
+                          objectFit: 'cover',
+                          width: '100%',
+
+                          height: 'auto',
+                        }}
+                      />
+                    </Box>
+
+                    <Typography
+                      component="h1"
+                      variant="h4"
+                      children={card.title}
+                      sx={{ marginBottom: '0.7em', color: 'yellow' }}
+                    />
+                    <Typography
+                      component="p"
+                      children={card.content}
+                      style={{ whiteSpace: 'pre-wrap' }}
                     />
                   </Box>
-                  <Typography component="h2" variant="" children={card.title} />
-                  <Typography
-                    component="p"
-                    variant=""
-                    children={card.content}
-                  />
                 </Box>
               </Grid>
             )
