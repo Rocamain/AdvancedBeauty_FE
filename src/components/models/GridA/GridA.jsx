109d1e9f717a510c1942f307e274968885de42f8
Rocamain <fjrocavazquez@gmail.com>
2022-07-12 14:00:47 +0100

    :styled(GridA): modify margins

--- src/components/models/GridA/GridA.jsx
+++ src/components/models/GridA/GridA.jsx
@@ -13,9 +13,16 @@ export default function GridA({
 }) {
   return (
     <Box
-      sx={{
-        margin: backgroundType === 'full' ? '25em auto 16em auto' : '0  auto',
-      }}
+      sx={(theme) => ({
+        mt:
+          backgroundType === 'full'
+            ? [theme.spacing(26), theme.spacing(34)]
+            : '0 auto',
+        mb:
+          backgroundType === 'full'
+            ? [theme.spacing(40), theme.spacing(48)]
+            : '0 auto',
+      })}
     >
       <Container background={backgroundType}>
         <Grid background={backgroundType} show={show} photoColumn={photoColumn}>
