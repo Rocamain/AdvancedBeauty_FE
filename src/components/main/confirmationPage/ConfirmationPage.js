7aa0f8a830f440c278d1ee42c7120aa7708b7b5c
Rocamain <fjrocavazquez@gmail.com>
2022-12-02 16:57:57 +0000

    finalize(Calendar):

--- src/components/main/confirmationPage/ConfirmationPage.js
+++ src/components/main/confirmationPage/ConfirmationPage.js
@@ -1,103 +1,140 @@
-import CardA from 'components/models/CardA/CardA.jsx';
 import { Box, Typography } from '@mui/material';
 import { useLocation } from 'react-router-dom';
-
-export default function ConfirmationPage({ props }) {
-  const { state } = useLocation();
-
-  const { serviceName, shopName, price, time, date, email, name } = state;
-  return (
-    <main style={{ minHeight: '50vh', height: '100%', marginBottom: '10vh' }}>
-      <CardA
-        sectionTitle={{ title: 'Booking Confirmation' }}
-        content={Content({ serviceName, shopName, price, time, date })}
-        bookingConfirmation={true}
-        confirmationMsg={ConfirmationMsg({ email, name })}
-      />
-    </main>
-  );
-}
+import { Card } from 'components/models/CardA/styled/index.js';
+import SectionTitle from 'components/shared/SectionTitle.jsx';
 
 const ConfirmationMsg = ({ name, email }) => {
   return (
-    <Box>
-      <Typography variant="cardText" sx={{ fontWeight: 600 }}>
-        {`Hi ${name}!!! Soon you will received a confirmation to your email:
-        ${email}`}
+    <Box sx={{ marginBottom: '2em' }}>
+      <Typography variant="cardText" component="p">
+        {`Hi ${name}!!! Soon you will received a confirmation to your email: ${email}`}
       </Typography>
     </Box>
   );
 };
 
-const Content = ({ serviceName, shopName, price, time, date }) => {
-  const dateString = `${date.$D}/${date.$D + 1}/${date.$y}`;
+const BookingDetails = ({ serviceName, shopName, price, time, date }) => {
+  const dateString = `${date.$D}/${date.$M}/${date.$y}`;
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-    >
+    <Box>
       <Typography
         color="primary"
-        variant="h5"
-        sx={{ textAlign: 'center', mb: '1em', fontWeight: 900 }}
+        variant="h2"
+        component="h3"
+        sx={{ textAlign: 'center', mb: '0.5em', fontWeight: 900 }}
       >
         Booking Details
       </Typography>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5em' }}>
-        <Box sx={{ display: 'flex', flexDirection: ['column', 'row'] }}>
-          <Typography
-            variant="h6"
-            color="tertiary"
-            sx={{ minWidth: '90px', fontWeight: 700 }}
-          >
-            Service:
-          </Typography>
-          <Typography variant="h6">{serviceName}</Typography>
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection: ['column', 'row'] }}>
-          <Typography
-            variant="h6"
-            color="tertiary"
-            sx={{ minWidth: '90px', fontWeight: 700 }}
-          >
-            Shop:
-          </Typography>
-          <Typography variant="h6">{shopName}</Typography>
-        </Box>
-        <Box sx={{ display: 'flex' }}>
-          <Typography
-            variant="h6"
-            color="tertiary"
-            sx={{ minWidth: '90px', fontWeight: 700 }}
-          >
-            Date:
-          </Typography>
-          <Typography variant="h6">{dateString}</Typography>
-        </Box>
-        <Box sx={{ display: 'flex' }}>
-          <Typography
-            variant="h6"
-            color="tertiary"
-            sx={{ minWidth: '90px', fontWeight: 700 }}
-          >
-            Time:
-          </Typography>
-          <Typography variant="h6">{time}</Typography>
-        </Box>
-        <Box sx={{ display: 'flex' }}>
-          <Typography
-            variant="h6"
-            color="tertiary"
-            sx={{ minWidth: '90px', fontWeight: 700 }}
-          >
-            Price:
-          </Typography>
-          <Typography variant="h6">{price} &#x20AC;</Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '1.5em',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            gap: '0.5em',
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              component="h6"
+              variant="cardText"
+              color="orange"
+              minWidth="100px"
+            >
+              Service:
+            </Typography>
+            <Typography component="p" variant="cardText">
+              {serviceName}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              component="h6"
+              variant="cardText"
+              color="orange"
+              minWidth="100px"
+            >
+              Shop:
+            </Typography>
+            <Typography component="p" variant="cardText">
+              {shopName}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              component="h6"
+              variant="cardText"
+              color="orange"
+              minWidth="100px"
+            >
+              Date:
+            </Typography>
+            <Typography component="p" variant="cardText">
+              {dateString}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              component="h6"
+              variant="cardText"
+              color="orange"
+              minWidth="100px"
+            >
+              Time:
+            </Typography>
+            <Typography component="p" variant="cardText">
+              {time}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              component="h6"
+              variant="cardText"
+              color="orange"
+              minWidth="100px"
+            >
+              Price:
+            </Typography>
+            <Typography component="p" variant="cardText">
+              {price} &#x20AC;
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </Box>
   );
 };
+
+export default function ConfirmationPage({ props }) {
+  const { state } = useLocation();
+  const { email, name, ...bookingDetails } = state;
+
+  return (
+    <main
+      style={{
+        paddingBottom: '10vh',
+        margin: '10vh 0',
+      }}
+    >
+      <Box
+        sx={{
+          margin: '0 auto',
+          width: ['90%', '80%', '65%'],
+        }}
+      >
+        <SectionTitle title="Booking Confirmation" grid cardA />
+        <ConfirmationMsg email={email} name={name} />
+        <Card card="booking">
+          <BookingDetails {...bookingDetails} />
+        </Card>
+      </Box>
+    </main>
+  );
+}
