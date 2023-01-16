b7235210ed771936ef192fbb58f995aaf5310313
Rocamain <fjrocavazquez@gmail.com>
2023-01-16 11:32:54 +0000

    finalize(navigation): adding head Title for document

---
+++ src/components/main/headTitle/HeadTitle.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const titleFormatter = (pathname) => {
+  const pathArray = pathname.split('/');
+  const paths = pathArray.filter((path) => path !== '');
+  const pathArrayLength = paths.length - 1;
+  let title = paths[pathArrayLength];
+
+  if (title) {
+    title = title.replaceAll('-', ' ');
+    title = title[0].toUpperCase() + title.slice(1);
+  }
+  return title ? title : 'Home';
+};
+
+export default function HeadTitle({ children, navigationLinks }) {
+  const { pathname } = useLocation();
+  const title = titleFormatter(pathname);
+
+  useEffect(() => {
+    if (pathname) {
+      document.title = `2U - ${title}`;
+    }
+  }, [pathname, title]);
+
+  return <>{children} </>;
+}
