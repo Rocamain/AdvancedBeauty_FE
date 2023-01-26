6d192e0ddd136dd49e69c500e898a169543d4ac0
Rocamain <fjrocavazquez@gmail.com>
2023-01-26 18:48:24 +0000

    Services retrieves data form loader context

--- src/components/main/section/section-components/Services/Services.jsx
+++ src/components/main/section/section-components/Services/Services.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 import { Typography, Box } from '@mui/material';
 import SectionTitle from 'components/shared/SectionTitle';
 import {
@@ -8,10 +9,9 @@ import {
 } from 'components/main/section/section-components/Services/styled/index';
 import { sortTreatments } from 'components/main/section/section-components/Services/utils/index';
 import Modal from 'components/main/booking/Modal/Modal';
-import useFetchBookingDb from 'hooks/useFetchBookingDb';
 
 export default function Services({ shopName, bookingAPI }) {
-  const services = useFetchBookingDb('getServices')?.services;
+  const { services } = useLoaderData();
 
   const [sectionExpanded, setSectionExpanded] = useState(null);
   const [serviceSelected, setServiceSelected] = useState(false);
@@ -27,96 +27,94 @@ export default function Services({ shopName, bookingAPI }) {
     setSectionExpanded(newExpanded ? event.target.innerText : false);
   };
 
-  if (services) {
-    const sortedTreatments = sortTreatments(services);
+  const sortedTreatments = sortTreatments(services);
 
-    return (
-      <Box
-        sx={{
-          width: ['100vw', '80vw', '80vw', '80vw', '70vw'],
-          margin: '0 auto',
-        }}
-      >
-        <SectionTitle title={`Treatments in ${shopName}`} left />
+  return (
+    <Box
+      sx={{
+        width: ['100vw', '80vw', '80vw', '80vw', '70vw'],
+        margin: '0 auto',
+      }}
+    >
+      <SectionTitle title={`Treatments in ${shopName}`} left />
 
-        {sortedTreatments.map(({ name, services }, index) => {
-          return (
-            <Accordion
-              key={index}
-              expanded={sectionExpanded === name}
-              onChange={handleChange}
+      {sortedTreatments.map(({ name, services }, index) => {
+        return (
+          <Accordion
+            key={index}
+            expanded={sectionExpanded === name}
+            onChange={handleChange}
+          >
+            <AccordionSummary
+              aria-controls={`panel-${name}-content`}
+              id={`panel-${name}-header`}
             >
-              <AccordionSummary
-                aria-controls={`panel-${name}-content`}
-                id={`panel-${name}-header`}
-              >
-                <Typography component="h4" variant="h4" sx={{ color: 'white' }}>
-                  {name}
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Box component={'ul'}>
-                  {services.map(({ price, serviceName }, index) => {
-                    return (
+              <Typography component="h4" variant="h4" sx={{ color: 'white' }}>
+                {name}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Box component={'ul'}>
+                {services.map(({ price, serviceName }, index) => {
+                  return (
+                    <Box
+                      key={index}
+                      id={serviceName}
+                      onClick={() => handleOpenModal(serviceName, price)}
+                      display="flex"
+                      component="li"
+                      sx={{
+                        width: 'inherit',
+                        gap: '1em',
+                      }}
+                    >
                       <Box
-                        key={index}
-                        id={serviceName}
-                        onClick={() => handleOpenModal(serviceName, price)}
                         display="flex"
-                        component="li"
+                        variant="p"
+                        component="p"
                         sx={{
-                          width: 'inherit',
-                          gap: '1em',
+                          width: '100%',
+                          justifyContent: 'space-between',
                         }}
                       >
-                        <Box
-                          display="flex"
+                        <Typography className="text" variant="p">
+                          {serviceName}
+                        </Typography>
+
+                        <Typography
+                          className="text"
+                          value={price}
                           variant="p"
-                          component="p"
                           sx={{
-                            width: '100%',
-                            justifyContent: 'space-between',
+                            whiteSpace: 'nowrap',
+                            display: 'inline-flex',
                           }}
                         >
-                          <Typography className="text" variant="p">
-                            {serviceName}
+                          {`${price},00`} &#x20AC;
+                          <Typography className="book" variant="p">
+                            Book
                           </Typography>
-
-                          <Typography
-                            className="text"
-                            value={price}
-                            variant="p"
-                            sx={{
-                              whiteSpace: 'nowrap',
-                              display: 'inline-flex',
-                            }}
-                          >
-                            {`${price},00`} &#x20AC;
-                            <Typography className="book" variant="p">
-                              Book
-                            </Typography>
-                          </Typography>
-                        </Box>
+                        </Typography>
                       </Box>
-                    );
-                  })}
-                </Box>
-              </AccordionDetails>
-            </Accordion>
-          );
-        })}
-        {serviceSelected && (
-          <Modal
-            open={isModalOpen}
-            handleClose={handleClose}
-            serviceName={serviceSelected}
-            shopName={shopName}
-            bookingAPI={bookingAPI}
-            price={servicePrice}
-            serviceType={sectionExpanded}
-          />
-        )}
-      </Box>
-    );
-  }
+                    </Box>
+                  );
+                })}
+              </Box>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
+      {serviceSelected && (
+        <Modal
+          open={isModalOpen}
+          handleClose={handleClose}
+          serviceName={serviceSelected}
+          shopName={shopName}
+          bookingAPI={bookingAPI}
+          price={servicePrice}
+          serviceType={sectionExpanded}
+        />
+      )}
+    </Box>
+  );
 }
