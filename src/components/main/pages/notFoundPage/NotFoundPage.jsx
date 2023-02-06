37d446495effc9ac55a84a430f295aa057658bcc
Rocamain <fjrocavazquez@gmail.com>
2023-02-06 15:18:14 +0000

    Add main HTML tag to components

--- src/components/main/pages/notFoundPage/NotFoundPage.jsx
+++ src/components/main/pages/notFoundPage/NotFoundPage.jsx
@@ -2,17 +2,19 @@ import { Box, Typography } from '@mui/material/';
 
 export default function NotFoundPage() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '40vh',
-      }}
-    >
-      <Typography component="h3" variant="h4" color="tertiary" bg="primary">
-        Page not found !!!
-      </Typography>
-    </Box>
+    <main>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '40vh',
+        }}
+      >
+        <Typography component="h3" variant="h4" color="tertiary" bg="primary">
+          Page not found !!!
+        </Typography>
+      </Box>
+    </main>
   );
 }
