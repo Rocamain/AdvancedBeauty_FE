189acb3bdf15c8da85d025832275a0df5b62d036
Rocamain <fjrocavazquez@gmail.com>
2024-05-14 15:48:08 +0100

    Fix exports

--- src/components/main/section/section-components/Form/utils/index.js
+++ src/components/main/section/section-components/Form/utils/index.js
@@ -1,11 +1,11 @@
-const validation = {
+export const validation = {
   name: (input) => input.length < 4,
   email: (input) => (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input) ? false : true),
   phone: (input) =>
     /^[-+/\s]*([0-9][-+/\s]*){9,}$/.test(input) ? false : true,
   message: (input) => input.length < 4,
 };
-const initialValues = {
+export const initialValues = {
   name: '',
   shop: '',
   email: '',
@@ -14,12 +14,10 @@ const initialValues = {
   result: '',
   checked: false,
 };
-const initialErrors = {
+export const initialErrors = {
   name: false,
   shop: false,
   email: false,
   phone: false,
   message: false,
 };
-
-module.exports = { initialErrors, initialValues, validation };
